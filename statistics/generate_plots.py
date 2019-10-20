44b73cfcc98beaf913594c3cdffca6ff2e07a346
Jennifer Schürch <jennifermschuerch@gmail.com>
2019-10-20 14:54:43 +0200

    Add box plot for time per move

--- statistics/generate_plots.py
+++ statistics/generate_plots.py
@@ -22,11 +22,13 @@ def main():
 def generatePlots(stats, title):
     data_scores=[]
     data_tiles=[]
+    data_time=[]
     tickLabels = []
     for stat in stats:
         stat['df'] = pd.read_csv(stat["csv"], sep=';')
         data_scores.append(stat['df']['score'].tolist())
         data_tiles.append(stat['df']['highest tile'].tolist())
+        data_time.append(stat['df']['time per move'].tolist())
         tickLabels.append(stat['name'])
 
     # Boxplots - Scores
@@ -42,6 +44,14 @@ def generatePlots(stats, title):
     ax1.set_title(title + " - Highest Tile")
     ax1.set_ylabel('Tile')
     ax1.boxplot(data_tiles)
+    plt.xticks(range(1,len(tickLabels)+1), tickLabels)
+    plt.show()
+
+    # Boxplots - Time
+    fig1, ax1 = plt.subplots()
+    ax1.set_title(title + " - Time per Move")
+    ax1.set_ylabel('Tile')
+    ax1.boxplot(data_time)
     plt.xticks(range(1,len(tickLabels)+1), tickLabels)
     plt.show()
 
