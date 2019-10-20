560b3b59ac822ffa6d18853345e41c88208de669
Jennifer Schürch <jennifermschuerch@gmail.com>
2019-10-20 13:30:27 +0200

    Add generalization

--- statistics/generate_plots.py
+++ statistics/generate_plots.py
@@ -10,21 +10,30 @@ searchai_stats = [
     { "name": "square", "csv": "searchai_square.csv", "df": []}
 ]
 
+heuristicai_stats = [
+    { "name": "heuristic", "csv": ".csv", "df": []},
+]
+
 def main():
+    generatePlots(searchai_stats, "Search AI")
+    #generatePlots(heuristicai_stats)
+
+def generatePlots(stats, title):
     data=[]
     tickLabels = []
-    for stat in searchai_stats:
+    for stat in stats:
         stat['df'] = pd.read_csv(stat["csv"], sep=';')
         data.append(stat['df']['score'].tolist())
         tickLabels.append(stat['name'])
 
     fig1, ax1 = plt.subplots()
-    ax1.set_title("Search AI - Scores")
+    ax1.set_title(title + " - Scores")
     ax1.set_ylabel('Score')
     ax1.boxplot(data)
 
     plt.xticks(range(1,len(tickLabels)+1), tickLabels)
     plt.show()
+
 
 if __name__ == '__main__':
     main();
