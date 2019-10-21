9bc9e05e084624f6beec8b2d80054a828742363d
Jennifer Schürch <jennifermschuerch@gmail.com>
2019-10-21 22:47:52 +0200

    Add measurements

--- statistics/generate_timeit_statistic.py
+++ statistics/generate_timeit_statistic.py
@@ -2,15 +2,15 @@
 
 searchai_times = [
     { "depth": 1, "timepermove": 0.010971},
-    { "depth": 2, "timepermove": 0.0123670},
-    { "depth": 3, "timepermove": 0},
-    { "depth": 4, "timepermove": 0},
-    { "depth": 5, "timepermove": 0},
+    { "depth": 2, "timepermove": 0.067817},
+    { "depth": 3, "timepermove": 2.558497},
+    { "depth": 4, "timepermove": 48.070847},
+    { "depth": 5, "timepermove": 1641.396178},
 ]
 searchai_times_multiprocess = [
-    { "depth": 1, "timepermove": 0},
-    { "depth": 2, "timepermove": 0},
-    { "depth": 3, "timepermove": 0},
-    { "depth": 4, "timepermove": 0},
-    { "depth": 5, "timepermove": 0},
+    { "depth": 1, "timepermove": 0.008644},
+    { "depth": 2, "timepermove": 0.040891},
+    { "depth": 3, "timepermove": 0.620329},
+    { "depth": 4, "timepermove": 24.369725},
+    { "depth": 5, "timepermove": 416.629117},
 ]
