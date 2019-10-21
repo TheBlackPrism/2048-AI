00ca7fa6984761196ec265563eba831c85ac5c24
Jennifer Schürch <jennifermschuerch@gmail.com>
2019-10-21 19:56:24 +0200

    Add statistics

---
+++ statistics/generate_timeit_statistic.py
@@ -0,0 +1,16 @@
+
+
+searchai_times = [
+    { "depth": 1, "timepermove": 0.010971},
+    { "depth": 2, "timepermove": 0.0123670},
+    { "depth": 3, "timepermove": 0},
+    { "depth": 4, "timepermove": 0},
+    { "depth": 5, "timepermove": 0},
+]
+searchai_times_multiprocess = [
+    { "depth": 1, "timepermove": 0},
+    { "depth": 2, "timepermove": 0},
+    { "depth": 3, "timepermove": 0},
+    { "depth": 4, "timepermove": 0},
+    { "depth": 5, "timepermove": 0},
+]
